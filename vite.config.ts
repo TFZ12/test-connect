6a14b5ced4d6dcbfd21bc4f951deac6208b516a8
tangfzngze <tangfangze@xdragoninfo.com>
2026-03-10 11:52:35 +0800

    feat：添加域名配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test-connect/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
